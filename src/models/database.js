12b63f035ed4fb71ead680825611713f24161117
Mihai <mihai.banulescu@gmail.com>
2016-08-03 04:47:30 -0700

    added pg-promise and CRUD operations for games model

--- src/models/database.js
+++
@@ -1,7 +0,0 @@
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/go-game-development';
-
-var client = new pg.Client(connectionString);
-client.connect();
-var query = client.query('CREATE TABLE games(id SERIAL PRIMARY KEY, board VARCHAR(90) not null, active BOOLEAN)');
-query.on('end', function() { client.end(); });
---
+++ src/models/initializeDatabase.js
@@ -0,0 +1,32 @@
+const bluebird = require('bluebird')
+const options = {
+    promiseLib: bluebird // overriding the default (ES6 Promise);
+}
+const pgp = require('pg-promise')(options)
+const connection = {
+  host: 'localhost',
+  port: 5432,
+  database: 'go-bison'
+}
+
+var db = pgp(connection)
+
+function sql(file) {
+  return new pgp.QueryFile(file, {minify: true})
+}
+
+var sqlInit = sql('./db.sql')
+
+const query = (string) => {
+  db.any(string)
+  .then(data => {
+    console.log('data', data)
+    return data
+  })
+  .catch((error) => {
+    console.log("ERROR:", error.message || error); // print error;
+  })
+  .finally(pgp.end())
+}
+
+export { query }
